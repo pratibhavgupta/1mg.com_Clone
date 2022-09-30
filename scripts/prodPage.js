7ba3b8da5f84a9cdefe1386b14c38e464e454841
Pratibhav Gupta <pra.pra.arp@gmail.com>
2022-09-30 23:28:41 +0530

    added

---
+++ scripts/prodPage.js
@@ -0,0 +1,14 @@
+// import {navbar} from "../navbar.js"
+// document.getElementById("navbar").innerHTML=navbar();
+
+// import {footer} from "../components/footer.js"
+// document.getElementById("footer").innerHTML=footer();
+
+let prod = {
+  img: "https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v1026-08-ktdpo2hf.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=762649fdf7d66f68f0d5fc1c694ce3ac",
+  title: "Wallpaper | Free Beautiful HD iPhone, Samsung & Mobile Phone ",
+  des: "Want to know where this information comes from? Learn more",
+};
+
+document.getElementById("myprod").src=prod.img;
+
